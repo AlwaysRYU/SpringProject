6fc7e1a4063c4e710204c385b2d1768ec8717fea
aliverise@naver.com <aliverise@naver.com>
2021-04-26 22:26:34 +0900

    2021.04.26. 스프링 프로젝트생성

---
+++ selfSpring/src/main/java/RYU/selfSpring/SelfSpringApplication.java
@@ -0,0 +1,14 @@
+package RYU.selfSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SelfSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfSpringApplication.class, args);
+	}
+
+}
---
+++ selfSpring/src/test/java/RYU/selfSpring/SelfSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package RYU.selfSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
