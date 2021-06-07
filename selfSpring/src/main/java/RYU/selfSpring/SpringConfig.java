f558204bc86975c1662fef2a5542d09eecd1ebef
aliverise@naver.com <aliverise@naver.com>
2021-06-07 17:14:17 +0900

    2021.06.07. H2 database와 JDBC 연결사용

--- selfSpring/src/main/java/RYU/selfSpring/SpringConfig.java
+++ selfSpring/src/main/java/RYU/selfSpring/SpringConfig.java
@@ -1,14 +1,24 @@
 package RYU.selfSpring;
 
+import RYU.selfSpring.repository.JdbcMemberRepository;
 import RYU.selfSpring.repository.MemberRepository;
 import RYU.selfSpring.repository.MemoryMemberRepository;
 import RYU.selfSpring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     //Springbean을 등록할거라는 의미
     @Bean
@@ -19,7 +29,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+
     }
 
 }
